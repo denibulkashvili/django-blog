24cc12257dec5287ce411bc9665e66511f8399ca
denibulkashvili <denibulkashvili@gmail.com>
2018-06-18 23:28:42 +0800

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    """Class representing posts"""
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    date_published = models.DateTimeField('date published')
+    topic = models.ForeignKey('Topic', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Topic(models.Model):
+    """Class representing topics"""
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+    
+
+
