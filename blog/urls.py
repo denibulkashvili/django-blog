e500e617db8d63c0a14a8ea40110395090acbc32
denibulkashvili <denibulkashvili@gmail.com>
2018-06-25 02:19:25 +0800

    Set up templates, and their views/urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+    path('topics/<topic_title>/', views.topic, name='topic'),
+    path('post/<post_title>/', views.post, name='post'),
+    # path('post/<int:post_id>/comment/', views.comment, name='comment'),
+    path('about/', views.about, name="about"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from .models import Topic, Post
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+    post_list = Post.objects.all().order_by('-date_published')
+    context = {'post_list': post_list}
+    return render(request, 'blog/index.html', context)
 
+def topics(request):
+    topic_list = Topic.objects.order_by('title')
+    context = {'topic_list': topic_list}
+    return render(request, 'blog/topics.html', context)
+
+def topic(request, topic_title):
+    posts = Post.objects.filter(topic__title=topic_title)
+    context = {'topic': topic_title, 'posts': posts}
+    return render(request, 'blog/topic.html', context)
+
+def post(request, post_title):
+    post = Post.objects.get(title=post_title)
+    context = {'post': post}
+    return render(request, 'blog/post.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+# def comment(request, post_id):
+#     return HttpResponse(f"You are commenting on post: {post_id}")
